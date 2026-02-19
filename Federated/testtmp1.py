f54bf379ab8533cfa39cf06da7e422566ae45dd9
S4r4v4n <120194541+s4r4v4n04@users.noreply.github.com>
2026-02-19 11:30:54 +0530

    Clean up testtmp1.py by removing unused imports
    Removed TensorFlow and TensorFlow Federated import statements and example code.

--- Federated/testtmp1.py
+++ Federated/testtmp1.py
@@ -1,9 +1,1 @@
 
-import tensorflow as tf
-print("TensorFlow version:", tf.__version__)
-
-#!pip install --upgrade tensorflow-federated
-
-import tensorflow_federated as tff
-print(tff.federated_computation(lambda: 'Hello World'))
-
